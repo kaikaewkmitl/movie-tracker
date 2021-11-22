2263de129a95de04cd646525d02fddb70b7a9db6
kaikaewkmitl <64011456@kmitl.ac.th>
2021-11-22 21:56:53 +0700

    allow user to toggle sort option in user list

--- pages/user_list_page.py
+++ pages/user_list_page.py
@@ -1,28 +1,55 @@
-from tkinter import Frame, Misc, Listbox, Scrollbar, messagebox
-from tkinter.constants import LEFT, RIGHT, BOTH, END
+from tkinter import Frame, Misc, Listbox, OptionMenu, Scrollbar, StringVar, messagebox
+from tkinter.constants import LEFT, RIGHT, BOTH, END, X
+from copy import deepcopy
 from typing import Callable, Optional
 
 from pages.abc_page import Page
 from tmdb_api.api import get_movie_by_id
-from utils.my_widgets import MyHeading, MyMediumFont
+from utils.my_widgets import MyHeading, MyMediumFont, MySmallFont
 from utils.globals import *
+
+LAST_ADDED = "Last Added"
+ALPHABETICAL = "Alphabetical"
+
+cur_sort_option = LAST_ADDED
 
 
 class UserListPage(Page):
     def __init__(self, parent: Misc, change_page_callback: Callable[[str, Optional[Dict[str, Any]]], None]) -> None:
         super().__init__(parent, change_page_callback)
+        self.__movie_list: List[Dict[str, Any]] = []
 
     def display(self) -> None:
         super().display()
 
         if len(store.user) > 0:
-            username = store.user["username"]
+            username = store.user[USER_USERNAME]
             user_heading = MyHeading(
                 self._page, text=f"{username}'s Movie List"
             )
             user_heading.pack()
 
-            movies = store.user[USER_MOVIE_LIST]
+            sort_option_container = Frame(self._page)
+            sort_option_container.pack(pady=10, padx=200, fill=X)
+
+            sort_option = StringVar(sort_option_container, cur_sort_option)
+
+            sort_option_dropdown = OptionMenu(
+                sort_option_container, sort_option,
+                LAST_ADDED, ALPHABETICAL,
+                command=self.dropdown_handler
+            )
+            sort_option_dropdown.pack(side=RIGHT)
+            sort_option_dropdown.config(font=MySmallFont())
+
+            sort_option_heading = MyHeading(
+                sort_option_container,
+                text="Sort By: ", font=MySmallFont()
+            )
+            sort_option_heading.pack(side=RIGHT, padx=10)
+
+            if len(self.__movie_list) == 0:
+                self.__movie_list = deepcopy(store.user[USER_MOVIE_LIST])
 
             movie_list_container = Frame(self._page)
             movie_list_container.pack(pady=20)
@@ -46,11 +73,11 @@ class UserListPage(Page):
             #                 ))
             movie_list.bind("<Double-Button-1>",
                             lambda _: self.listbox_handler(
-                                movies[movie_list.curselection()[0]]
+                                self.__movie_list[movie_list.curselection()[0]]
                             ))
             movie_list.pack(side=LEFT)
 
-            for movie in movies:
+            for movie in self.__movie_list:
                 movie_list.insert(
                     END, movie[MOVIE_TITLE]
                 )
@@ -80,3 +107,14 @@ class UserListPage(Page):
         self._change_page_cb(
             MOVIE_INFO_PAGE, retrieved_movie
         )
+
+    def dropdown_handler(self, option):
+        global cur_sort_option
+        if option == LAST_ADDED and cur_sort_option != LAST_ADDED:
+            self.__movie_list = deepcopy(store.user[USER_MOVIE_LIST])
+            cur_sort_option = LAST_ADDED
+            self._change_page_cb(USER_LIST_PAGE)
+        elif option == ALPHABETICAL and cur_sort_option != ALPHABETICAL:
+            self.__movie_list.sort(key=lambda m: m[MOVIE_TITLE])
+            cur_sort_option = ALPHABETICAL
+            self._change_page_cb(USER_LIST_PAGE)
